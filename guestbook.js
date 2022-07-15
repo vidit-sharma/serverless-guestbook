91480bbbe2d98b207391266ff71ee0533dc943db
Vidit Vivek Sharma <88590113+vidit-sharma@users.noreply.github.com>
2022-07-14 22:30:03 -0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://825f610d.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
